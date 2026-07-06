901e5a6eb8971c9a3a3d6e08f0c1ad16f5e6c9f1
Soty Dev <devsoty@Sotonyes-Mac.local>
2026-07-06 11:56:56 +0100

    code clean up before PR merge of v1

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -4,32 +4,29 @@ from app.models import TutorProfile, User
 from app import db
 from . import admin
 
+#You can still add flash messages to these when you are designing it
+
 @admin.route('/application')
 @login_required
 def index():
     if not current_user.is_admin:
         abort(403)
     else:
-        print("Hello World")
         applications = TutorProfile.query.filter_by(is_approved=False).all()
         
     return render_template('admin/index.html', applications=applications)
 
 @admin.route('/approve/<int:tutor_id>', methods=['POST'])
 def approve(tutor_id):
-    print(tutor_id)
     profile = db.session.get(TutorProfile, tutor_id)
     profile.user.is_tutor = True
     profile.is_approved = True
     db.session.commit()
-    print("Approval Done!")
     return redirect(url_for('admin.index'))
 
 @admin.route('/reject/<int:tutor_id>', methods=['POST'])
 def reject(tutor_id):
-    print(tutor_id)
     profile = db.session.get(TutorProfile, tutor_id)
     db.session.delete(profile)
     db.session.commit()
-    print(profile)
     return redirect(url_for('admin.index'))
